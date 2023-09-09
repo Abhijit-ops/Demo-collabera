c7a3644658c86f556b70c4858f4f84fad6f1793a
AbhijitKalita <abhijitkalita1984@gmail.com>
2023-09-09 16:47:30 +0530

    added terraform code

---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "vpccidr" {
+    default = "192.168.0.0/20"
+}
+variable "publiccidr01" {
+    default = "192.168.0.0/22"
+}
+variable "publiccidr02" {
+    default = "192.168.4.0/22"
+}
+variable "privatecidr01" {
+    default = "192.168.8.0/22"
+}
+variable "privatecidr02" {
+    default = "192.168.12.0/22"
+}
